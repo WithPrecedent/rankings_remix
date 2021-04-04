b26ea523f354f15881c1b76ea064769cecf6871f
WithPrecedent <coreyrayburnyung@gmail.com>
2021-04-04 13:52:06 -0500

    finalized visuals

--- rankings/remix.py
+++ rankings/remix.py
@@ -6,7 +6,6 @@ License: Apache-2.0 (https://www.apache.org/licenses/LICENSE-2.0)
 
 """
 from __future__ import annotations
-import copy
 import pathlib
 from typing import (Any, Callable, ClassVar, Dict, Hashable, Iterable, List, 
     Mapping, MutableMapping, MutableSequence, Optional, Sequence, Set, Tuple, 
@@ -19,9 +18,10 @@ import seaborn as sns
 from sklearn import preprocessing
 
 DATA_FOLDER = pathlib.Path('..') / 'data' / 'rankings' 
+VISUALS_FOLDER = pathlib.Path('..') / 'visuals' / 'rankings'
 IMPORT_DATA_PATH = pathlib.Path(DATA_FOLDER) / '2022rankings.csv'
 EXPORT_DATA_PATH = pathlib.Path(DATA_FOLDER) / 'remixed_rankings.csv'
-VISUALS_FOLDER = pathlib.Path('..') / 'visuals' / 'rankings' 
+ 
 RENAMES = { 
     'Rank': 'US News Rank',
     'Overall score': 'US News Score',
@@ -78,19 +78,6 @@ CORE_COLUMNS = [
     'LSAT Estimated Median',
     '10-Month Employed',
     'Average Debt']    
-RANK_COMPARISON_COLUMNS = [
-    # 'US News Rank',
-    # 'School',
-    'Hidden Data Rank Boost',
-    'Standardization Rank Boost',
-    'Questionable Data Categories Rank Boost']
-SCORE_COMPARISON_COLUMNS = [
-    # 'US News Rank',
-    # 'School',
-    # 'US News Score',
-    'Hidden Data Score Boost',
-    'Standardization Score Boost',
-    'Questionable Data Categories Score Boost']
 
 def import_rankings_data() -> pd.DataFrame:
     df = pd.read_csv(IMPORT_DATA_PATH, encoding = 'windows-1252')
@@ -100,8 +87,9 @@ def import_rankings_data() -> pd.DataFrame:
 def fix_lower_ranks(df: pd.DataFrame) -> pd.DataFrame:
     df['US News Rank'] = df['US News Rank'].str.split('-').str[0]
     df['US News Rank'] = pd.to_numeric(df['US News Rank'], downcast = 'integer')
-    df['US News Score Scaled'] = minmax_scale(df = df, column = 'US News Score')
-    df['US News Rank Scaled'] = minmax_scale(df = df, column = 'US News Rank', low_is_good = True)
+    df['US News Score Scaled'] = minmax_scale(data = df['US News Score'])
+    df['US News Rank Scaled'] = minmax_scale(data = df['US News Rank'], 
+                                             low_is_good = True)
     return df
 
 def change_to_numerical(df: pd.DataFrame) -> pd.DataFrame:
@@ -122,7 +110,9 @@ def change_to_numerical(df: pd.DataFrame) -> pd.DataFrame:
                     df[high] = pd.to_numeric(df[high])
                     df[median] = df[[low, high]].mean(axis = 1)
                 elif kind in [float, 'percent']:
-                    df[column] = df[column].str.split('%').str[0].astype('float')
+                    df[column] = (df[column].str.split('%')
+                                            .str[0]
+                                            .astype('float'))
                     if kind in ['percent']:
                         df[column] = df[column]/100
     return df
@@ -132,22 +122,20 @@ def compute_bar_ratio(df: pd.DataFrame) -> pd.DataFrame:
         df['Bar Pass Rate'] / df['Jurisdiction Bar Pass Rate'])
     return df
 
-def ordinal_rank(df: pd.DataFrame, column: str, method: str = 'max') -> pd.Series:
+def ordinal_rank(df: pd.DataFrame, 
+                 column: str, 
+                 method: str = 'max') -> pd.Series:
     return df[column].rank(method = method)
 
-def minmax_scale(df: pd.DataFrame, column: str, low_is_good: bool = False) -> pd.Series:
+def minmax_scale(data: pd.Series, low_is_good: bool = False) -> pd.Series:
     if low_is_good:
-        column_data = pd.Series(1 - df[column])
-    else:
-        column_data = df[column]
-    return (column_data - column_data.min()) / (column_data.max() - column_data.min())
+        data = pd.Series(1 - data)
+    return preprocessing.minmax_scale(data)
 
-def standard_scale(df: pd.DataFrame, column: str, low_is_good: bool = False) -> pd.Series:
+def standard_scale(data: pd.Series, low_is_good: bool = False) -> pd.Series:
     if low_is_good:
-        column_data = pd.Series(1 - df[column])
-    else:
-        column_data = df[column]
-    return preprocessing.scale(column_data)
+        data = pd.Series(1 - data)
+    return preprocessing.scale(data)
 
 scalers = {
     'Percent': minmax_scale,
@@ -160,7 +148,7 @@ def scale_all_usnews_columns(df: pd.DataFrame) -> pd.DataFrame:
             if column in LOW_IS_BETTER:
                 kwargs['low_is_good'] = True
             scaled_column = f'{name} {column}'
-            df[scaled_column] = scaler(df = df, column = column, **kwargs)
+            df[scaled_column] = scaler(data = df[column], **kwargs)
     return df 
 
 def compute_scores(df: pd.DataFrame) -> pd.DataFrame:   
@@ -172,13 +160,14 @@ def compute_scores(df: pd.DataFrame) -> pd.DataFrame:
         scaled_score_column = f'{score_column} Scaled'
         weights = list(USNEWS_WEIGHTS.values())
         df[score_column] = df[columns].mul(weights).sum(1)
-        df[scaled_score_column] = minmax_scale(df = df, column = score_column)
+        df[scaled_score_column] = minmax_scale(data = df[score_column])
     core_columns = [col for col in df if col.startswith('Percent')]
-    core_columns = [col for col in core_columns if col.endswith(tuple(CORE_COLUMNS))]
+    core_columns = [
+        col for col in core_columns if col.endswith(tuple(CORE_COLUMNS))]
     core_weights = {k: v for k, v in USNEWS_WEIGHTS.items() if k in CORE_COLUMNS}
     core_weights = list(core_weights.values())
     df['Core Score'] = df[core_columns].mul(core_weights).sum(1)
-    df['Core Score Scaled'] = minmax_scale(df = df, column = 'Core Score')
+    df['Core Score Scaled'] = minmax_scale(data = df['Core Score'])
     return df
 
 def compute_ranks(df: pd.DataFrame) -> pd.DataFrame:
@@ -190,30 +179,47 @@ def compute_ranks(df: pd.DataFrame) -> pd.DataFrame:
         rank_column = f'{name} Rank'
         df[rank_column] = df[score_column].rank(method = 'min', ascending = False)
     df['Core Rank'] = df['Core Score'].rank(method = 'min', ascending = False) 
-    df['US News Rank Adjusted'] = df['US News Rank'].rank(method = 'min', ascending = True) 
+    df['US News Rank Adjusted'] = df['US News Rank'].rank(method = 'min', 
+                                                          ascending = True) 
     return df
 
 def add_comparisons(df: pd.DataFrame) -> pd.DataFrame:
-    df['Hidden Data Rank Boost'] = df['Standardized Rank'] - df['US News Rank Adjusted']
-    df['Standardization Rank Boost'] = df['Percent Rank'] - df['Standardized Rank Adjusted']
-    df['Questionable Data Categories Rank Boost'] = df['Core Rank'] - df['Percent Rank Adjusted']
-    df['Hidden Data Score Boost'] = df['US News Score Scaled'] - df['Standardized Score Scaled'] 
-    df['Standardization Score Boost'] = df['Standardized Score Scaled'] - df['Percent Score Scaled'] 
-    df['Questionable Data Categories Score Boost'] = df['Percent Score Scaled'] - df['Core Score Scaled'] 
+    df['Hidden Data Rank Boost'] = (df['Standardized Rank'] 
+                                    - df['US News Rank Adjusted'])
+    df['Standardization Rank Boost'] = (df['Percent Rank'] 
+                                        - df['Standardized Rank'])
+    df['Questionable Data Categories Rank Boost'] = (df['Core Rank'] 
+                                                     - df['Percent Rank'])
+    df['Hidden Data Score Boost'] = (df['US News Score Scaled'] 
+                                     - df['Standardized Score Scaled']) 
+    df['Standardization Score Boost'] = (df['Standardized Score Scaled'] 
+                                         - df['Percent Score Scaled'])
+    df['Questionable Data Categories Score Boost'] = (df['Percent Score Scaled'] 
+                                                      - df['Core Score Scaled']) 
     return df
 
 def visualize_standardization_effects(df: pd.DataFrame) -> None:
     percent_columns = [f'Percent {key}' for key in USNEWS_WEIGHTS]
     standardized_columns = [f'Standardized {key}' for key in USNEWS_WEIGHTS]
     column_groups = [percent_columns, standardized_columns]
-    distributions, axes = plt.subplots(figsize = (12, 6), ncols = len(column_groups))
-    distributions.suptitle('Effects of Standardization on Component Score Distributions', size = 20)
+    distributions, axes = plt.subplots(figsize = (12, 6), 
+                                       ncols = len(column_groups), 
+                                       sharey = True)
+    distributions.suptitle(
+        'Effects of Standardization on Component Score Distributions', 
+        size = 16)
     for i, column_group in enumerate(column_groups):
         for column in column_group:
-            distributions = sns.kdeplot(df[column], ax = axes[i], legend = False)
+            distributions = sns.kdeplot(df[column], 
+                                        ax = axes[i], 
+                                        legend = False)
     axes[0].set_xlim([0, 1])
     axes[1].set_xlim([-3, 3])
-    plt.legend(title = 'Categories', bbox_to_anchor = (1.01, 1), borderaxespad = 0, labels = list(USNEWS_WEIGHTS.keys()), prop = {'size': 10})
+    plt.legend(title = 'Categories', 
+               bbox_to_anchor = (1.01, 1), 
+               borderaxespad = 0, 
+               labels = list(USNEWS_WEIGHTS.keys()), 
+               prop = {'size': 10})
     plt.tight_layout()
     export_path = pathlib.Path(VISUALS_FOLDER) / 'category_distributions.png'
     distributions.figure.savefig(export_path)
@@ -222,30 +228,65 @@ def visualize_standardization_effects(df: pd.DataFrame) -> None:
   
 def visualize_score_rank_distributions(df: pd.DataFrame) -> None:
     distributions, axis = plt.subplots()
-    distributions.suptitle('Distributions of US News Scores and Rankings', size = 20)
-    sns.histplot(df['US News Score Scaled'], ax = axis, color = 'blue', kde = True, label = 'US News Score')
-    sns.histplot(df['US News Rank Scaled'], ax = axis, color = 'orange', kde = True, label = 'US News Rank')
+    distributions.suptitle('Distributions of US News Scores and Rankings', 
+                           size = 16)
+    sns.histplot(df['US News Score Scaled'], 
+                 ax = axis, 
+                 color = 'blue', 
+                 kde = True, 
+                 label = 'US News Score')
+    sns.histplot(df['US News Rank Scaled'], 
+                 ax = axis, 
+                 color = 'orange', 
+                 kde = True, 
+                 label = 'US News Rank')
     axis.set(xlabel = 'US News Scores and Ranks (common scale)')
     axis.set_xlim([0, 1])
+    plt.tight_layout()
     export_path = pathlib.Path(VISUALS_FOLDER) / 'score_rank_distributions.png'
     distributions.figure.savefig(export_path)
     plt.close()
     return
 
 def visualize_comparisons(df: pd.DataFrame) -> None:
-    public_scatter = sns.scatterplot(x = df['Standardized Rank'], y = df['US News Rank'], color = 'green')
+    public_scatter = sns.scatterplot(x = df['Standardized Rank'], 
+                                     y = df['US News Rank'], 
+                                     color = 'green')
     plt.ylabel('US News Public Data Rank')
-    public_scatter.set_xlim([df['US News Rank'].min(), df['US News Rank'].max()])
-    public_scatter.set_ylim([df['US News Rank'].min(), df['US News Rank'].max()])
+    public_scatter.set_xlim([df['US News Rank'].min(), 
+                             df['US News Rank'].max()])
+    public_scatter.set_ylim([df['US News Rank'].min(), 
+                             df['US News Rank'].max()])
+    public_scatter.figure.suptitle(
+        'Effects of Hidden Data on School Rankings', 
+        size = 16)
+    plt.tight_layout()
     export_path = pathlib.Path(VISUALS_FOLDER) / 'public_scatter.png'
     public_scatter.figure.savefig(export_path)
     plt.close()
-    score_comparison = sns.scatterplot(x = df['US News Score Scaled'], y = df['US News Score'], label = 'US News', color = 'orange', alpha = 0.35)
-    score_comparison = sns.scatterplot(x = df['Percent Score Scaled'], y = df['US News Score'], label = 'Percent', color = 'olive', alpha = 0.5)
-    score_comparison = sns.scatterplot(x = df['Core Score Scaled'], y = df['US News Score'], label = 'Core Metrics', color = 'purple', alpha = 0.35)
+    score_comparison = sns.scatterplot(x = df['US News Score Scaled'], 
+                                       y = df['US News Score'], 
+                                       label = 'US News', 
+                                       color = 'orange', 
+                                       alpha = 0.35)
+    score_comparison = sns.scatterplot(x = df['Percent Score Scaled'], 
+                                       y = df['US News Score'], 
+                                       label = 'Percent', 
+                                       color = 'olive', 
+                                       alpha = 0.5)
+    score_comparison = sns.scatterplot(x = df['Core Score Scaled'], 
+                                       y = df['US News Score'], 
+                                       label = 'Core Metrics', 
+                                       color = 'purple', 
+                                       alpha = 0.35)
     score_comparison.set_xlim(0, 1)
-    score_comparison.set_ylim([df['US News Score'].min() - 5, df['US News Score'].max() + 5])
-    plt.xlabel('Alternative Scores')
+    score_comparison.set_ylim([df['US News Score'].min() - 5, 
+                               df['US News Score'].max() + 5])
+    plt.xlabel('Percent Score')
+    score_comparison.figure.suptitle(
+        'Comparison of Different Scoring Methodologies', 
+        size = 16)
+    plt.tight_layout()
     export_path = pathlib.Path(VISUALS_FOLDER) / 'score_comparison.png'
     score_comparison.figure.savefig(export_path)
     plt.close()
@@ -268,8 +309,9 @@ if __name__ == '__main__':
     complete_df = import_rankings_data()
     complete_df = fix_lower_ranks(df = complete_df)
     only_ranked_df = complete_df[complete_df['US News Rank'] < 145]
-    df = complete_df.dropna()
-    # df = df[df['US News Rank'] < 112]
+    check_na_columns = list(RENAMES.values())
+    check_na_columns = check_na_columns.remove('US News Score')
+    df = complete_df.dropna(subset = check_na_columns)
     df = change_to_numerical(df = df)
     df = compute_bar_ratio(df = df)
     df = scale_all_usnews_columns(df = df)
